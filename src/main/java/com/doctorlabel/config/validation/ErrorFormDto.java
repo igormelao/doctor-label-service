f9d62dba09f5f8eb53ec606becdd5dc07654f789
Igor Melão <igormelao@gmail.com>
2021-03-07 19:54:43 +0000

    Create handler error for bad requests

---
+++ src/main/java/com/doctorlabel/config/validation/ErrorFormDto.java
@@ -0,0 +1,21 @@
+package com.doctorlabel.config.validation;
+
+public class ErrorFormDto {
+
+	private String field;
+	private String message;
+
+	public ErrorFormDto(String field, String message) {
+		this.field = field;
+		this.message = message;
+	}
+
+	public String getField() {
+		return field;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+}
