66febe0384f887c89327ddeb74923ba2c3d5b0b7
Igor Melão <igormelao@gmail.com>
2021-03-13 19:43:04 +0000

    Organize Cors configuration into a new class inside of package security

--- src/main/java/com/doctorlabel/DoctorlabelApplication.java
+++ src/main/java/com/doctorlabel/DoctorlabelApplication.java
@@ -3,9 +3,6 @@ package com.doctorlabel;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
@@ -19,18 +16,5 @@ public class DoctorlabelApplication {
 	
 	@Value("${allowed.origin}")
 	private String allowedOrigin;
-	
-	@Bean
-	public WebMvcConfigurer corsConfigurer() {
-		return new WebMvcConfigurer() {
-			@Override
-			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**")
-				.allowedOrigins(allowedOrigin, "http://localhost:8080", "http://localhost:8081")
-				.allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE")
-				.allowedHeaders("*");
-			}
-		};
-	}
 
 }
