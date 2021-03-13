94e1dd3ac581df0a5605035c4939f5dfe7e8ea16
Igor Melão <igormelao@gmail.com>
2021-03-13 12:08:49 +0000

    Change the configurations Sprig Boot MVC to use ability CORS for front-end calls; Chane the name of the serve on applicaiton properties;

--- src/main/java/com/doctorlabel/DoctorlabelApplication.java
+++ src/main/java/com/doctorlabel/DoctorlabelApplication.java
@@ -1,13 +1,33 @@
 package com.doctorlabel;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class DoctorlabelApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DoctorlabelApplication.class, args);
+	}
+	
+	@Value("${allowed.origin}")
+	private String allowedOrigin;
+	
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+				.allowedOrigins(allowedOrigin, "http://localhost:8080", "http://localhost:8081")
+				.allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE")
+				.allowedHeaders("*");
+			}
+		};
 	}
 
 }
