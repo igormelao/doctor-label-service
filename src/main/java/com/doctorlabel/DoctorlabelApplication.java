33149a46e2c5c1bbb9254cd041b18acbac9d497e
Igor Melão <igormelao@gmail.com>
2021-03-13 19:11:39 +0000

    Instal the project Spring Fox Swagger to generate all API documentation automatically

--- src/main/java/com/doctorlabel/DoctorlabelApplication.java
+++ src/main/java/com/doctorlabel/DoctorlabelApplication.java
@@ -7,7 +7,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 @SpringBootApplication
+@EnableSwagger2
 public class DoctorlabelApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/doctorlabel/config/swagger/SwaggerConfigurations.java
@@ -0,0 +1,24 @@
+package com.doctorlabel.config.swagger;
+
+import java.util.Arrays;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import springfox.documentation.builders.ParameterBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.schema.ModelRef;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+@Configuration
+public class SwaggerConfigurations {
+
+	@Bean
+	public Docket api() {
+		return new Docket(DocumentationType.SWAGGER_2).select()
+				.apis(RequestHandlerSelectors.basePackage("com.doctorlabel")).paths(PathSelectors.ant("/**")).build();
+	}
+
+}
